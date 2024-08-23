bc12d3c3a73657853a28e682cf89f26890b8f6db
HughOwenPanopio <panopiohughowen@gmail.com>
2024-08-23 22:05:02 +0800

    added leading brands component

--- src/components/leadingbrands/LeadingBrands.jsx
+++ src/components/leadingbrands/LeadingBrands.jsx
@@ -1,15 +1,17 @@
 import cannaWholesalers from '../../assets/cannaWholesalers.png'
+import destinoFarms from '../../assets/destinoFarms.png'
+import teeter from "../../assets/teeter.png"
 import './style.css'
 
 function LeadingBrands() {
   return (
     <div>
-      <div className="container brandContent">
-        <div className="row">
+      <div className="container-fluid brandContent">
+        <div className="row brands">
           <div className="col brandText">trusted by leading cannabis brands</div>
-          <div className="col">test</div>
+          <div className="col"><img src={teeter} alt="teeter" className='teeter' /></div>
           <div className="col"><img src={cannaWholesalers} alt="cannaWholesalers" className='cannaWholesalers' /></div>
-          <div className="col">test</div>
+          <div className="col"><img src={destinoFarms} alt="destinoFarms" className='destinoFarms' /></div>
         </div>
       </div>
     </div>
