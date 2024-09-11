6b2b0976c592a38b7d8c10cddd390a1fb7ba236e
HughOwenPanopio <panopiohughowen@gmail.com>
2024-09-11 11:12:30 +0800

    reverting to the default settings of burger menu

--- src/common/Header.jsx
+++ src/common/Header.jsx
@@ -5,11 +5,11 @@ import blossomtreeslogo from '../assets/blossomtreeslogo.png'
 
 function Header() {
 
-  const [isNotDesktop, setIsNotDesktop] = useState(window.innerWidth < 1024)
+  const [isNotDesktop, setIsNotDesktop] = useState(window.innerWidth < 1025)
 
   useEffect(function () {
     const handleResize = () => {
-      setIsNotDesktop(window.innerWidth < 1024)
+      setIsNotDesktop(window.innerWidth < 1025)
     }
     window.addEventListener('resize', handleResize)
 
