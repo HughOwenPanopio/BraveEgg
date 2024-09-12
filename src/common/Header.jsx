004b2e87d621cb3fb6c6db8caa121c6b0a9c4540
HughOwenPanopio <panopiohughowen@gmail.com>
2024-09-12 14:20:14 +0800

    fixed the burger menu on viewport 1274px for ipad air 4

--- src/common/Header.jsx
+++ src/common/Header.jsx
@@ -5,11 +5,11 @@ import blossomtreeslogo from '../assets/blossomtreeslogo.png'
 
 function Header() {
 
-  const [isNotDesktop, setIsNotDesktop] = useState(window.innerWidth < 1025)
+  const [isNotDesktop, setIsNotDesktop] = useState(window.innerWidth < 1274)
 
   useEffect(function () {
     const handleResize = () => {
-      setIsNotDesktop(window.innerWidth < 1025)
+      setIsNotDesktop(window.innerWidth < 1274)
     }
     window.addEventListener('resize', handleResize)
 
